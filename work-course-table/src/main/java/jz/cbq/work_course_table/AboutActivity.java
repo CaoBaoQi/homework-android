5546ea52db3ee247b675cee6c47853d408aaf2fd
曹宝琪 <2023cbq@gmail.com>
2023-11-22 01:42:26 +0800

    course-table: error

---
+++ work-course-table/src/main/java/jz/cbq/work_course_table/AboutActivity.java
@@ -0,0 +1,17 @@
+package jz.cbq.work_course_table;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Lenovo on 2018/12/1.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        }
+}
---
+++ work-course-table/src/main/java/jz/cbq/work_course_table/SpinnerActivity.java
@@ -0,0 +1,31 @@
+package jz.cbq.work_course_table;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+/**
+ * Created by LENOVO on 2018/12/21.
+ */
+
+public class SpinnerActivity extends AppCompatActivity {
+    private Spinner mWeek = null;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_course);
+        mWeek = (Spinner) findViewById(R.id.week_choose);
+        String[] arr = {"单周", "双周", "每周"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.week_list, arr);
+        mWeek.setAdapter(adapter);
+        mWeek.setOnItemClickListener((AdapterView.OnItemClickListener)this);
+    }
+}
+
