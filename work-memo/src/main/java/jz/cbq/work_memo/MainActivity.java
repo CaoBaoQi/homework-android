169e4066b751261eb04f8b55d0b3c8e661cb61c3
曹宝琪 <2023cbq@gmail.com>
2023-11-21 23:44:47 +0800

    memo: init commit

---
+++ work-memo/src/main/java/jz/cbq/work_memo/MainActivity.java
@@ -0,0 +1,14 @@
+package jz.cbq.work_memo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
