ea0546f9b9d4ac599911789d7914a23e1bed96a5
曹宝琪 <2023cbq@gmail.com>
2023-11-22 00:51:29 +0800

    course-table: init commit

---
+++ work-course-table/src/main/java/jz/cbq/work_course_table/MainActivity.java
@@ -0,0 +1,14 @@
+package jz.cbq.work_course_table;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
